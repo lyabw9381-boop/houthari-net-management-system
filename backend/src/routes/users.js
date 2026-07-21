5dd096681877cb9e217366e439c2544c9bcda535
lyabw9381-boop <lyabw9381@gmail.com>
2026-07-21 06:14:35 +0300

    Update users.js

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -2,40 +2,83 @@ const express = require("express");
 
 const router = express.Router();
 
+const roles = require("../config/roles");
+
+
 let users = [
+
     {
         id:1,
         name:"مدير النظام",
         username:"admin",
         password:"123456",
         role:"admin"
+    },
+
+    {
+        id:2,
+        name:"موظف خدمة العملاء",
+        username:"employee",
+        password:"123456",
+        role:"employee"
+    },
+
+    {
+        id:3,
+        name:"محصل",
+        username:"collector",
+        password:"123456",
+        role:"collector"
+    },
+
+    {
+        id:4,
+        name:"الدعم الفني",
+        username:"support",
+        password:"123456",
+        role:"support"
     }
+
 ];
 
 
+
 // عرض المستخدمين
+
 router.get("/", (req,res)=>{
+
     res.json(users);
+
 });
 
 
+
+
 // تسجيل الدخول
+
 router.post("/login",(req,res)=>{
 
+
     const user = users.find(
+
         u =>
         u.username === req.body.username &&
         u.password === req.body.password
+
     );
+
 
 
     if(!user){
 
         return res.status(401).json({
+
             message:"بيانات الدخول غير صحيحة"
+
         });
 
     }
+
 
 
     res.json({
@@ -43,13 +86,21 @@ router.post("/login",(req,res)=>{
         message:"تم تسجيل الدخول",
 
         user:{
+
             name:user.name,
-            role:user.role
+
+            role:user.role,
+
+            permissions:
+            roles[user.role].permissions
+
         }
 
     });
 
+
 });
+
 
 
 module.exports = router;
